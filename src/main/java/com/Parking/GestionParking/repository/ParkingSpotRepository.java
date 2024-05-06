7feff7e83323198cd70bd0dc4db52b536f08869c
Anas Hammou <80295074+Anas-Hammou@users.noreply.github.com>
2024-05-06 10:36:20 +0100

    bug correction (avaible spot now working 100 %)
    avaible spot now working 100 %

--- src/main/java/com/Parking/GestionParking/repository/ParkingSpotRepository.java
+++ src/main/java/com/Parking/GestionParking/repository/ParkingSpotRepository.java
@@ -1,10 +1,11 @@
 package com.Parking.GestionParking.repository;
 
-
 import com.Parking.GestionParking.entities.ParkingSpot;
 import com.Parking.GestionParking.entities.SpotType;
+import com.Parking.GestionParking.entities.Reservation;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
@@ -12,10 +13,21 @@ import java.util.List;
 
 @Repository
 public interface ParkingSpotRepository extends CrudRepository<ParkingSpot, Long> {
-    @Query("SELECT ps FROM ParkingSpot ps LEFT JOIN ps.reservations r WHERE ps.spotType = :spotType AND " +
-            "((r.date < :endDate AND r.date > :startDate) OR (r.duration < :endDate AND r.duration > :startDate) OR " +
-            "(r.date > :startDate AND r.duration < :endDate) OR (r.date < :startDate AND r.duration > :endDate) OR " +
-            "(r.date = :startDate AND r.duration = :endDate)) " +
-            "GROUP BY ps HAVING COUNT(r) = 0")
-    List<ParkingSpot> findAvailableSpotsByTypeAndTime(Date startDate, Date endDate, SpotType spotType);
+    @Query("SELECT ps FROM ParkingSpot ps " +
+            "WHERE ps.spotType = :spotType AND " +
+            "ps.idParkingSpot NOT IN (SELECT DISTINCT r.parkingSpot.idParkingSpot " +
+            "FROM Reservation r " +
+            "WHERE (" +
+            "(r.date < :endDate AND r.date > :startDate) OR " +
+            "(r.duration < :endDate AND r.duration > :startDate) OR " +
+            "(r.date > :startDate AND r.duration < :endDate) OR " +
+            "(r.date < :startDate AND r.duration > :endDate) OR " +
+            "(r.date >= :startDate AND r.date < :endDate) OR " + // Reservation starts within the specified range
+            "(r.duration > :startDate AND r.duration <= :endDate)" + // Reservation ends within the specified range
+            "))")
+    List<ParkingSpot> findAvailableSpotsByTypeAndTime(
+            @Param("startDate") Date startDate,
+            @Param("endDate") Date endDate,
+            @Param("spotType") SpotType spotType);
+
 }
